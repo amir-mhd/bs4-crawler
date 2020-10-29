ad961d40b569fd71534356ab00a8109bc2128235
amir-mhd <amirmohammadian@hotmail.com>
2020-10-29 22:40:40 +0330

    adding the post_link attr to scrape

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -14,21 +14,23 @@ posts = parsed_html.find_all("div", class_="card-body")
 # writting the scraped data to a csv file
 data_csv_file = open("data.csv", "w")
 csv_writer = csv.writer(data_csv_file)
-csv_writer.writerow(["title", "summary"])
+csv_writer.writerow(["title", "summary", "post link"])
 
 # use for loop to iterate over every post
 for post in posts:
     try:
         title = post.h3.a.text
         summary = post.find("div", class_="post-cut").p.text 
+        href = post.find("a", class_="btn")["href"]
+        post_link = f"https://romanvm.pythonanywhere.com/{href}"
     except Exception:
-        pass
-    
+        continue
     print(f"-- {title} --")
     print(summary)
+    print(f"++ {post_link} ++")
     print()
 
-    csv_writer.writerow([title, summary])
+    csv_writer.writerow([title, summary, post_link])
 
 
 data_csv_file.close()
