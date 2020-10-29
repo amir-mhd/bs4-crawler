393fb380e6ec554e38e396ee99c57c2754d43bcb
amir-mhd <amirmohammadian@hotmail.com>
2020-10-29 11:38:35 +0330

    fixing lxml parser issue

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,5 +1,11 @@
 from bs4 import BeautifulSoup  
 import requests
+import lxml
 
+html_source = requests.get("https://news.ycombinator.com/news").text
+
+soup = BeautifulSoup(html_source, 'html.parser')
+
+print(soup.prettify())
 
 
