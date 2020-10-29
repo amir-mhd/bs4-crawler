5ec2d22526fbfaeaf2f226ea984083061d8f0a8f
amir-mhd <amirmohammadian@hotmail.com>
2020-10-29 21:41:17 +0330

    writting the data to csv, adding try and except block

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup  
 import requests
+import csv
 
 # sending request to the target website
 html_source = requests.get("https://romanvm.pythonanywhere.com/").text
@@ -10,10 +11,24 @@ parsed_html = BeautifulSoup(html_source, 'html.parser')
 # specify a section of the entire html
 posts = parsed_html.find_all("div", class_="card-body")
 
+# writting the scraped data to a csv file
+data_csv_file = open("data.csv", "w")
+csv_writer = csv.writer(data_csv_file)
+csv_writer.writerow(["title", "summary"])
+
 # use for loop to iterate over every post
 for post in posts:
-    title = post.h3.a.text
-    paragraph = post.find("div", class_="post-cut").p.text 
+    try:
+        title = post.h3.a.text
+        summary = post.find("div", class_="post-cut").p.text 
+    except Exception:
+        pass
+    
     print(f"-- {title} --")
-    print(paragraph)
+    print(summary)
     print()
+
+    csv_writer.writerow([title, summary])
+
+
+data_csv_file.close()
