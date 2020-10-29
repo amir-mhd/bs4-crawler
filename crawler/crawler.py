ba5e9e428dfd8af6225dbad3654f47af78964209
amir-mhd <amirmohammadian@hotmail.com>
2020-10-29 17:39:59 +0330

    parsing and printing some selected section of the website

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -2,10 +2,19 @@ from bs4 import BeautifulSoup
 import requests
 import lxml
 
-html_source = requests.get("https://news.ycombinator.com/news").text
+# sending request to the target website
+html_source = requests.get("https://romanvm.pythonanywhere.com/").text
 
-soup = BeautifulSoup(html_source, 'html.parser')
+# parse the html from the target website and store it 
+parsed_html = BeautifulSoup(html_source, 'html.parser')
 
-print(soup.prettify())
+# specify a section of the entire html
+posts = parsed_html.find_all("div", class_="card-body")
 
-
+# use for loop to iterate over every post
+for post in posts:
+    title = post.h3.a.text
+    paragraph = post.find("div", class_="post-cut").p.text 
+    print(f"-- {title} --")
+    print(paragraph)
+    print()
